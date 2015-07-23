477f1d539e65e58dc31f8371d4a70a9cf93111b8
Raphael Knaus <rknuus@gmail.com>
2015-07-23 21:50:37 +0200

    add basic crack method

--- jemmy/reverse.py
+++ jemmy/reverse.py
@@ -1,3 +1,12 @@
+from collections import namedtuple
+
+
 class Reverse:
     def decrypt(self, ciphertext, **kwargs):
         return ciphertext[::-1]
+
+    def crack(self, ciphertext, **kwargs):
+        result = namedtuple('Result', 'cracked plaintext')
+        result.cracked = True
+        result.plaintext = self.decrypt(ciphertext, **kwargs)
+        return result
--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -1,21 +1,34 @@
-import unittest
+from collections import namedtuple
 from nose.tools import *
 import jemmy.reverse
+import unittest
 
 
 class TestReverse(unittest.TestCase):
     def test_reverse_empty_word(self):
-        r = jemmy.reverse.Reverse()
-        self.assertEqual(r.decrypt(''), '')
+        plugin = jemmy.reverse.Reverse()
+        self.assertEqual(plugin.decrypt(''), '')
 
     def test_reverse_one_letter_word(self):
-        r = jemmy.reverse.Reverse()
-        self.assertEqual(r.decrypt('a'), 'a')
+        plugin = jemmy.reverse.Reverse()
+        self.assertEqual(plugin.decrypt('a'), 'a')
 
     def test_reverse_two_letter_word(self):
-        r = jemmy.reverse.Reverse()
-        self.assertEqual(r.decrypt('ab'), 'ba')
+        plugin = jemmy.reverse.Reverse()
+        self.assertEqual(plugin.decrypt('ab'), 'ba')
 
     def test_reverse_sentence(self):
-        r = jemmy.reverse.Reverse()
-        self.assertEqual(r.decrypt('I prefer Ruby over Python.'), '.nohtyP revo ybuR referp I')
+        plugin = jemmy.reverse.Reverse()
+        self.assertEqual(plugin.decrypt('I prefer Ruby over Python.'), '.nohtyP revo ybuR referp I')
+
+    def test_crack_empty_word(self):
+        plugin = jemmy.reverse.Reverse()
+        result = plugin.crack('')
+        self.assertEqual(result.cracked, True)
+        self.assertEqual(result.plaintext, '')
+
+    def test_crack_two_letter_word(self):
+        plugin = jemmy.reverse.Reverse()
+        result = plugin.crack('ba')
+        self.assertEqual(result.cracked, True)
+        self.assertEqual(result.plaintext, 'ab')
