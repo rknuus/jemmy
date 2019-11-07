2a5a861175d2d0341257731be9ad955abd46da8f
Raphael Knaus <rknuus@gmail.com>
2019-11-07 21:06:19 +0100

    Cover reverse encryption by tests

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -5,21 +5,25 @@ import jemmy.reverse
 
 def test_reverse_empty_word():
     plugin = jemmy.reverse.Reverse(jemmy.dic.Dic(['a']))
+    assert plugin.encrypt('') == ''
     assert plugin.decrypt('') == ''
 
 
 def test_reverse_one_letter_word():
     plugin = jemmy.reverse.Reverse(jemmy.dic.Dic(['a']))
+    assert plugin.encrypt('a') == 'a'
     assert plugin.decrypt('a') == 'a'
 
 
 def test_reverse_two_letter_word():
     plugin = jemmy.reverse.Reverse(jemmy.dic.Dic(['a']))
+    assert plugin.encrypt('ab') == 'ba'
     assert plugin.decrypt('ab') == 'ba'
 
 
 def test_reverse_sentence():
     plugin = jemmy.reverse.Reverse(jemmy.dic.Dic(['a']))
+    assert plugin.encrypt('.nohtyP revo ybuR referp I') == 'I prefer Ruby over Python.'
     assert plugin.decrypt('I prefer Ruby over Python.') == '.nohtyP revo ybuR referp I'
 
 
